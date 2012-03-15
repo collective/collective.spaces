0da51772a1f030c32bf70e2a16952b4cdd14049f
David\ Beitey <david@davidjb.com>
2012-03-15 13:53:51 +1000

    Adding id field for Spaces schema

--- src/collective/spaces/space.py
+++ src/collective/spaces/space.py
@@ -1,4 +1,6 @@
-from plone.directives import dexterity, form
+from zope import schema
+from plone.app.dexterity.interfaces import isValidId
+from plone.directives import form
 from plone.namedfile.field import NamedImage
 
 from collective.spaces import _
@@ -7,6 +9,15 @@ from collective.spaces import _
 class ISpace(form.Schema):
     """Virtual collaboration workspace that acts as a navigation root.
     """
+
+    form.order_before(id='*')
+    id = schema.ASCIILine(
+        title=_(u"Short Name"),
+        description=_(u"Should not contain spaces, underscores or mixed case. "
+                      "Short Name is part of the item's web address."),
+        required=False,
+        constraint=isValidId
+    )
 
     custom_logo = NamedImage(
         title=_(u"Custom Logo"),
