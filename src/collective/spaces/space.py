2d5586cf24bdba1c2c50eee620db24997ae31a6d
David\ Beitey <david@davidjb.com>
2012-03-20 13:45:19 +1000

    Using custom type class so we can get DAV-aware creation

--- src/collective/spaces/space.py
+++ src/collective/spaces/space.py
@@ -1,7 +1,10 @@
+from five import grok
 from zope import schema
+from plone.dexterity.content import Container
 from plone.app.dexterity.interfaces import isValidId
 from plone.directives import form
 from plone.namedfile.field import NamedImage
+from Products.GenericSetup.interfaces import IDAVAware
 
 from collective.spaces import _
 
@@ -26,3 +29,5 @@ class ISpace(form.Schema):
     )
 
 
+class Space(Container):
+    grok.implements(IDAVAware)
