68764b0d238b55da5fc0d8a48071dee389b61ac4
David\ Beitey <david@davidjb.com>
2012-03-20 13:37:56 +1000

    Adding more tests for installation checking

--- src/collective/spaces/tests/test_install.py
+++ src/collective/spaces/tests/test_install.py
@@ -16,10 +16,25 @@ class TestInstall(unittest.TestCase):
         self.qi_tool = getToolByName(self.portal, 'portal_quickinstaller')
 
     def test_product_is_installed(self):
-        """ Validate that our products GS profile has been run and the product
+        """ Validate that our product's GS profile has been run and the product
             installed
         """
         pid = 'collective.spaces'
         installed = [p['id'] for p in self.qi_tool.listInstalledProducts()]
         self.assertTrue(pid in installed,
                         'package appears not to have been installed')
+
+    def test_permissions_installed(self):
+        """ Ensure permissions are available and set correctly.
+        """
+        try:
+            self.portal.rolesOfPermission('collective.spaces: Add Space')
+            self.portal.rolesOfPermission('collective.spaces: Create Space via web')
+        except:
+            self.fail('Permissions not available')
+
+    def test_structure_created(self):
+        from collective.spaces.space import ISpace
+
+        self.assertIn('space-template', self.portal)
+        self.assertTrue(ISpace.providedBy(self.portal['space-template']))
