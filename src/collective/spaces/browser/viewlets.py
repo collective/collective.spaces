3a181b0623afdd765b7be685e5e017d5b5cf5bc9
David\ Beitey <david@davidjb.com>
2012-03-22 12:09:27 +1000

    Adding tests for logo viewlets

--- src/collective/spaces/browser/viewlets.py
+++ src/collective/spaces/browser/viewlets.py
@@ -19,6 +19,65 @@ class CustomLogoViewlet(common.LogoViewlet):
 
 
 class LogoViewlet(common.LogoViewlet):
+    """ Logo viewlet used to override Plone's default logo.
+
+    This viewlet will utilise Plone's default logo unless the current
+    navigation root is a Space and has a custom logo applied to it.
+
+        >>> import transaction
+        >>> from plone.testing.z2 import Browser
+
+        >>> app = layer['app']
+        >>> portal = layer['portal']
+        >>> space = layer['space']
+        >>> browser = Browser(app)
+
+    Check the default portal logo is being used in non-Space areas.
+
+        >>> browser.open(portal.absolute_url())
+        >>> browser.getLink(id='portal-logo')
+        <Link text='Plone site[IMG]' ...
+        >>> '<img src="http://nohost/plone/logo.png"' in browser.contents
+        True
+
+    Visit the Space and this should be the same for now.
+
+        >>> browser.open(space.absolute_url())
+        >>> browser.getLink(id='portal-logo')
+        <Link text='Plone site[IMG]' ...
+        >>> '<img src="http://nohost/plone/logo.png"' in browser.contents
+        True
+
+    Here's a simple PNG logo to use as the Space's custom logo. This is just
+    a 1x1 black PNG. Set this up as the logo.
+
+        >>> from plone import namedfile
+
+        >>> logo = "\\x89PNG\\r\\n\\x1a\\n\\x00\\x00\\x00\\rIHDR\\x00\\x00\\x00\\x01\\x00\\x00\\x00\\x01\\x08\\x02\\x00\\x00\\x00\\x90wS\\xde\\x00\\x00\\x00\\x01sRGB\\x00\\xae\\xce\\x1c\\xe9\\x00\\x00\\x00\\x0cIDAT\\x08\\xd7c```\\x00\\x00\\x00\\x04\\x00\\x01'4'\\n\\x00\\x00\\x00\\x00IEND\\xaeB`\\x82"
+        >>> space.custom_logo = namedfile.NamedImage(logo, filename=u"1x1.png")
+        >>> transaction.commit()
+
+    Check to see this is now visible
+
+        >>> browser.reload()
+        >>> browser.getLink(id='portal-logo')
+        <Link text='My wonderful Space[IMG]' url='.../test-space'>
+        >>> '<img src="http://nohost/plone/test-space/@@images/' \\
+        ...     in browser.contents
+        True
+
+    Sanity check to ensure this logo doesn't go and escape to anywhere else
+    on the site.
+
+        >>> browser.open(portal.absolute_url())
+        >>> '<img src="http://nohost/plone/logo.png"' in browser.contents
+        True
+
+        >>> browser.open(portal['test-space-2'].absolute_url())
+        >>> '<img src="http://nohost/plone/logo.png"' in browser.contents
+        True
+
+    """
     index = ViewPageTemplateFile('logo.pt')
 
     def update(self):
---
+++ src/collective/spaces/tests/test_viewlets.py
@@ -0,0 +1,40 @@
+import unittest2 as unittest
+import doctest
+import transaction
+
+import zope
+from z3c.form.interfaces import IValidator
+from plone.app.dexterity.interfaces import InvalidIdError
+from plone.testing import layered
+from plone.testing.z2 import Browser
+from plone.app.testing import setRoles, \
+        TEST_USER_ID, TEST_USER_PASSWORD
+
+from collective.spaces.testing import COLLECTIVE_SPACES_FUNCTIONAL_TESTING, \
+        DOC_TEST_OPTIONS
+
+def _setUp(test):
+    layer = test.globs['layer']
+    portal = layer['portal']
+    setRoles(portal, TEST_USER_ID, ['Manager'])
+    portal.invokeFactory('collective.spaces.space',
+                         id='test-space',
+                         title='My wonderful Space')
+    layer['space'] = portal['test-space']
+    portal.invokeFactory('collective.spaces.space',
+                         id='test-space-2',
+                         title='Another Space')
+    setRoles(portal, TEST_USER_ID, ['Member'])
+    transaction.commit()
+
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTests([
+        layered(
+            doctest.DocTestSuite('collective.spaces.browser.viewlets',
+                                 optionflags=DOC_TEST_OPTIONS,
+                                 setUp=_setUp),
+            layer=COLLECTIVE_SPACES_FUNCTIONAL_TESTING
+        ),
+    ])
+    return suite
