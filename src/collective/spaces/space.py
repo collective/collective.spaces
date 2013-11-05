bcdc5d87d4dc85741ddd10bfc6897b59c87b38f8
David\ Beitey <david@davidjb.com>
2013-11-06 08:53:16 +1000

    Workaround folder type restriction for AT content inside Dexterity content. See https://dev.plone.org/ticket/13792

--- src/collective/spaces/space.py
+++ src/collective/spaces/space.py
@@ -1,7 +1,17 @@
 from five import grok
 from plone.dexterity.content import Container
 from Products.GenericSetup.interfaces import IDAVAware
+from plone.app.dexterity.behaviors.constrains import ConstrainTypesBehavior
 
 
 class Space(Container):
     grok.implements(IDAVAware)
+
+    #Fixes for AT-based content inside a Dexterity container
+    def getLocallyAllowedTypes(self, context=None):
+        behavior = ConstrainTypesBehavior(self)
+        return behavior.getLocallyAllowedTypes(context)
+
+    def getImmediatelyAddableTypes(self, context=None):
+        behavior = ConstrainTypesBehavior(self)
+        return behavior.getImmediatelyAddableTypes(context)
